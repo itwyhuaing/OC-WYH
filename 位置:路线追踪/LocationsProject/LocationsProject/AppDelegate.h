5f2b3b254b09b6a4f08a5d1d2d0246f08d82330b
wangyinghua <itwyhuaing@163.com>
2018-01-07 22:54:29 +0800

    地图定位与追踪

---
+++ 位置:路线追踪/LocationsProject/LocationsProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationsProject
+//
+//  Created by wangyinghua on 2018/1/7.
+//  Copyright © 2018年 ZhiXing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 位置:路线追踪/LocationsProject/LocationsProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocationsProject
+//
+//  Created by wangyinghua on 2018/1/7.
+//  Copyright © 2018年 ZhiXing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
