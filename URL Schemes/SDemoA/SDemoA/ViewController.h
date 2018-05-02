61331ffd4c35b9ed79361ab560d01712215ccafc
hnbwyh <rainwang@hinabian.com>
2018-05-02 15:19:11 +0800

    问题修改描述：URL scheme 笔记补充 检视人：rain 验证人：hanckcai 该问题第几次上库：1

---
+++ URL Schemes/SDemoA/SDemoA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDemoA
+//
+//  Created by hnbwyh on 2018/5/2.
+//  Copyright © 2018年 ZhiXingJY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
