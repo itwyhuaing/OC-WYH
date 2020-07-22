680c7ebaf5ba9013b3167259f0cddc8ea9b53241
hnbwyh <rainwang@hinabian.com>
2020-07-22 15:48:57 +0800

    笔记补充。

---
+++ C_安全与逆向/加密解密/SafetyDemo/SafetyDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SafetyDemo
+//
+//  Created by hnbwyh on 2020/7/22.
+//  Copyright © 2020 JiXia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
